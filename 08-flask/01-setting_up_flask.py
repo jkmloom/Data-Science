be48a44bebaf6a5cbc67ef242ef42dc6b078ead0
Jatin Kumar Mehta <jatinmehtagaya@gmail.com>
2026-06-08 23:08:26 +0530

    updated 01-setting_up_flask.py

--- 08-flask/01-setting_up_flask.py
+++ 08-flask/01-setting_up_flask.py
@@ -2,9 +2,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+
 @app.route('/')
 def home():
+    print('home page!!')
     return "welcome to the flask app!"
 
 if __name__ == "__main__":
+    print('entering into the applicaiton')
     app.run(debug=True)
